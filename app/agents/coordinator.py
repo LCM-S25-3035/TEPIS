7a2373e6b7a09fbf7e598dc02c770b98373e26a6
veragustavo119 <sangsunlee.aiden@gmail.com>
2025-08-12 10:37:19 -0400

    fi

--- app/agents/coordinator.py
+++ app/agents/coordinator.py
@@ -26,8 +26,8 @@ else:
     from .transportation_agent import TransportationAgent
 
 def get_secret():
-    a = "hf_FlXnWyOVnahDcZb"
-    b = "bQDOiXzyrTeESiDykIH"
+    a = "hf_nLTEfUlIDDgnhm"
+    b = "DTCCjkBqWFLKHRmPtCVy"
     return a+b
 class ItineraryCoordinator:
     def __init__(self, event_data):
